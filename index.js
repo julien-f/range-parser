d0443701b985e16fcd6399830be2b60a15a21a4c
Julien Fontanet <julien.fontanet@isonoe.net>
2016-12-01 15:13:34 +0100

    feat: use standard

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 // UMD: https://github.com/umdjs/umd/blob/master/returnExports.js
 (function (root, factory) {
+  /* globals define:false */
+
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define([], factory);
+    define([], factory)
   } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory();
+    module.exports = factory()
   } else {
     // Browser globals (root is window)
-    root.rangeParser = factory();
+    root.rangeParser = factory()
   }
 }(this, function () {
-  'use strict';
+  'use strict'
 
-  //==================================================================
+  // ==================================================================
 
   var is = (function () {
     var is = {
@@ -23,9 +25,9 @@
       //   return ((value !== null) && (typeof value === 'object'));
       // },
       integer: function (value) {
-        return is.number(value) && (value % 1 === 0);
-      },
-    };
+        return is.number(value) && (value % 1 === 0)
+      }
+    }
 
     var types = {
       // arguments: (function () { return arguments; })(),
@@ -36,110 +38,98 @@
       number: 0,
       // plainObject: {},
       // regexp: /(?:)/,
-      string: '',
+      string: ''
       // undefined: undefined,
-    };
+    }
 
-    var has = Object.prototype.hasOwnProperty;
-    has = has.call.bind(has);
-    var toS = Object.prototype.toString;
-    toS = toS.call.bind(toS);
+    var has = Object.prototype.hasOwnProperty
+    has = has.call.bind(has)
+    var toS = Object.prototype.toString
+    toS = toS.call.bind(toS)
 
-    for (var type in types)
-    {
-      if (has(types, type))
-      {
+    for (var type in types) {
+      if (has(types, type)) {
         is[type] = (function (tag) {
           return function (value) {
-            return (toS(value) === tag);
-          };
-        })(toS(types[type]));
+            return (toS(value) === tag)
+          }
+        })(toS(types[type]))
       }
     }
 
-    return is;
-  })();
+    return is
+  })()
 
-  //==================================================================
+  // ==================================================================
 
   var parseEntry = function (entry) {
-    if (entry && is.string(entry))
-    {
-      entry = +entry;
+    if (entry && is.string(entry)) {
+      entry = +entry
     }
 
-    if (!is.integer(entry))
-    {
-      throw new Error('invalid entry: '+ entry);
+    if (!is.integer(entry)) {
+      throw new Error('invalid entry: ' + entry)
     }
 
-    return entry;
-  };
+    return entry
+  }
 
   var parseRange = function (range, results) {
-    var i, n;
+    var i, n
 
-    range = range.split('-');
-    n = range.length;
+    range = range.split('-')
+    n = range.length
 
-    if (n === 1)
-    {
-      results.push(parseEntry(range[0]));
-      return;
+    if (n === 1) {
+      results.push(parseEntry(range[0]))
+      return
     }
 
-    if (n !== 2)
-    {
-      throw new Error('invalid range: '+ range);
+    if (n !== 2) {
+      throw new Error('invalid range: ' + range)
     }
 
-    i = parseEntry(range[0]);
-    n = parseEntry(range[1]);
+    i = parseEntry(range[0])
+    n = parseEntry(range[1])
 
-    if (i >= n)
-    {
-      throw new Error('invalid range: '+ i +'-'+ n);
+    if (i >= n) {
+      throw new Error('invalid range: ' + i + '-' + n)
     }
 
-    for (; i <= n; ++i)
-    {
-      results.push(i);
+    for (; i <= n; ++i) {
+      results.push(i)
     }
-  };
+  }
 
   var parseUnion = function (union) {
-    var i, n, results;
-    results = [];
-    union = union.split(',');
-    for (i = 0, n = union.length; i < n; ++i)
-    {
-      parseRange(union[i], results);
+    var i, n, results
+    results = []
+    union = union.split(',')
+    for (i = 0, n = union.length; i < n; ++i) {
+      parseRange(union[i], results)
     }
-    return results;
-  };
+    return results
+  }
 
   var clean = function (range) {
-    if (is.number(range))
-    {
-      range = ''+ range;
-    }
-    else if (!is.string(range))
-    {
-      throw new Error('invalid range: '+ range);
+    if (is.number(range)) {
+      range = '' + range
+    } else if (!is.string(range)) {
+      throw new Error('invalid range: ' + range)
     }
 
-    return range;
-  };
+    return range
+  }
 
   var parser = function (union) {
-    return parseUnion(clean(union));
-  };
+    return parseUnion(clean(union))
+  }
   parser.withoutUnions = function (range) {
-    range = clean(range);
-    var results = [];
-    parseRange(range, results);
-    return results;
-  };
+    range = clean(range)
+    var results = []
+    parseRange(range, results)
+    return results
+  }
 
-  return parser;
-}));
+  return parser
+}))
--- index.spec.js
+++ index.spec.js
@@ -1,78 +1,80 @@
-'use strict';
+'use strict'
 
-//====================================================================
+/* eslint-env mocha */
 
-var parseRange = require('./');
+// ====================================================================
 
-//--------------------------------------------------------------------
+var parseRange = require('./')
 
-var each = require('lodash').each;
-var expect = require('chai').expect;
+// --------------------------------------------------------------------
 
-//====================================================================
+var each = require('lodash').each
+var expect = require('chai').expect
+
+// ====================================================================
 
 var data = {
   entry: {
     'as a string': [
       '1',
-      [1],
+      [1]
     ],
     'as a number': [
       1,
-      [1],
-    ],
+      [1]
+    ]
   },
   range: {
     'basic': [
       '1-3',
-      [1, 2, 3],
-    ],
+      [1, 2, 3]
+    ]
   },
   union: {
     'of entries': [
       '1,3',
-      [1, 3],
+      [1, 3]
     ],
     'of ranges': [
       '1-3,5-7',
-      [1, 2, 3, 5, 6, 7],
+      [1, 2, 3, 5, 6, 7]
     ],
     'of entries and ranges': [
       '1-3,9,5-7,11',
-      [1, 2, 3, 9, 5, 6, 7, 11],
-    ],
-  },
-};
+      [1, 2, 3, 9, 5, 6, 7, 11]
+    ]
+  }
+}
 
-//--------------------------------------------------------------------
+// --------------------------------------------------------------------
 
 var succeed = function (parser, type) {
   each(data[type], function (data, title) {
-    it('successfully parse a '+ type +' '+ title, function () {
-      expect(parser(data[0])).to.have.members(data[1]);
-    });
-  });
-};
+    it('successfully parse a ' + type + ' ' + title, function () {
+      expect(parser(data[0])).to.have.members(data[1])
+    })
+  })
+}
 
 var fail = function (parser, type) {
   each(data[type], function (data, title) {
-    it('fails to parse a '+ type +' '+ title, function () {
+    it('fails to parse a ' + type + ' ' + title, function () {
       expect(function () {
-        parser(data[0]);
-      }).to.throw(Error);
-    });
-  });
-};
+        parser(data[0])
+      }).to.throw(Error)
+    })
+  })
+}
 
 describe('parseRange()', function () {
-  succeed(parseRange, 'entry');
-  succeed(parseRange, 'range');
-  succeed(parseRange, 'union');
+  succeed(parseRange, 'entry')
+  succeed(parseRange, 'range')
+  succeed(parseRange, 'union')
 
   describe('.withoutUnions()', function () {
-    succeed(parseRange.withoutUnions, 'entry');
-    succeed(parseRange.withoutUnions, 'range');
+    succeed(parseRange.withoutUnions, 'entry')
+    succeed(parseRange.withoutUnions, 'range')
 
-    fail(parseRange.withoutUnions, 'union');
-  });
-});
+    fail(parseRange.withoutUnions, 'union')
+  })
+})
